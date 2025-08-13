bd7e126388fb7ab3ce86a9c9a929d9f2ead519af
Akash Baghel <akash27aug.2002@gmail.com>
2025-08-13 11:58:15 +0530

    xm

--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -18,7 +18,7 @@ export const AppContextProvider = (props) => {
     const {user} = useClerk()
     const [products, setProducts] = useState([])
     const [userData, setUserData] = useState(false)
-    const [isSeller, setIsSeller] = useState(true)
+    const [isSeller, setIsSeller] = useState(false)
     const [cartItems, setCartItems] = useState({})
 
     const fetchProductData = async () => {
@@ -26,7 +26,14 @@ export const AppContextProvider = (props) => {
     }
 
     const fetchUserData = async () => {
-        setUserData(userDummyData)
+        try {
+            if(user.publicMetadata.role === 'seller'){
+                setIsSeller(true);
+            }
+            setUserData(userDummyData);
+        } catch (error) {
+            console.log(error.message);
+        }
     }
 
     const addToCart = async (itemId) => {
@@ -80,8 +87,10 @@ export const AppContextProvider = (props) => {
     }, [])
 
     useEffect(() => {
-        fetchUserData()
-    }, [])
+        if(user){
+            fetchUserData()
+        }
+    }, [user])
 
     const value = {
         user,
